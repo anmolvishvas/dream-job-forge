c6fa05ce144c0f26818b48b721be1c2389f52d24
Anmol Vishvas <anmol23vishvas@gmail.com>
2025-05-11 14:42:57 +0400

    build with low memory

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -2,15 +2,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-export default defineConfig(({}) => ({
+export default defineConfig({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+  build: {
+    target: "es2015", // Lower target means less transpilation work
+    minify: false, // Avoid minification to save memory
+    cssCodeSplit: false, // Reduce output complexity
+    sourcemap: false, // Don't generate source maps
+  },
+});
